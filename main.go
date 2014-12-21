03b3e30171eaba73a7a52ad1d019735bc88502b5
Sam Nguyen <sam.nguyen@sendgrid.com>
2014-12-21 11:28:10 -0800

    WIP: next bookmark

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/codegangsta/cli"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/codegangsta/cli"
 )
 
 const (
@@ -16,9 +18,53 @@ const (
 	version    = "0.0.1"
 )
 
+var (
+	configFile = os.Getenv("HOME") + "/.bible"
+)
+
+type config struct {
+	Bookmarks map[string]string `toml:"bookmarks"`
+}
+
+func readConfig() *config {
+	c := config{}
+	_, err := os.Stat(configFile)
+
+	if os.IsNotExist(err) {
+		log.Println("ReadConfig", err)
+		c.Bookmarks["next"] = "Gen 1"
+		return &c
+	}
+
+	_, err = toml.DecodeFile(configFile, &c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &c
+}
+
+func (c *config) write() error {
+	f, err := os.Create(configFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	enc := toml.NewEncoder(f)
+	return enc.Encode(c)
+}
+
+func nextRef(ref string) string {
+	return ref
+}
+
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	conf := readConfig()
+
 	app := cli.NewApp()
-	app.Name = "ment"
+	app.Name = "bible"
 	app.Usage = "a command-line tool for reading the Bible"
 	app.Author = "Sam Nguyen"
 	app.Email = "samxnguyen@gmail.com"
@@ -30,16 +76,25 @@ func main() {
 			ShortName: "r",
 			Usage:     "Read a passage",
 			Action: func(c *cli.Context) {
+				var ref = strings.Join([]string(c.Args()), " ")
+				if strings.ToLower(ref) == "next" {
+					ref = conf.Bookmarks["next"]
+
+					conf.Bookmarks["next"] = nextRef(ref)
+					conf.write()
+				}
+
 				query := url.Values{
 					"key":                        {"IP"},
 					"output-format":              {"plain-text"},
-					"passage":                    {strings.Join([]string(c.Args()), " ")},
+					"passage":                    {ref},
 					"include-headings":           {"0"},
 					"include-subheadings":        {"0"},
 					"include-passage-references": {"0"},
 					"include-verse-numbers":      {"0"},
 					"include-footnotes":          {"0"},
 				}
+
 				resp, err := http.Get(esvBaseURL + "/passageQuery?" +
 					query.Encode())
 				if err != nil {
